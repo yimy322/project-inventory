a351a611228124c0d49f3b132ed8d7f9991c2bd3
yimy <fh2001yj@gmail.com>
2024-09-22 20:11:12 -0500

    se corrigieron los comentarios

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -3,17 +3,17 @@ package connection;
 import java.sql.*;
 
 public class ConnectionDB {
-//  Cadena de conexion con mysql
+//  cadena de conexion con mysql, los parametros puestos son para evitar ciertos errores que pueden aparecer
 	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/inventory?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-//  Aca especificamos el usuario
+//  especificamos el usuario
 	private static final String JDBC_USER = "root";
-//  Aca la contraseña de mysql
+//  aca la contrasena de mysql
 	private static final String JDBC_PASSWORD = "root@322-ABC";
-//  Metodo para obtener la conexion a al base de datos
+//  obtenemos la conexion de la base de datos
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
 	}
-//  cierra la conexion
+//  cerramos la conexion
 	public static void close(Connection conn){
 		if (conn != null) {
             try {
