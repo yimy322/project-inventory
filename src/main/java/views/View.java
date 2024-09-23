20e0e22bf30f76671c5bb41ef49803418980fd1f
yimy <fh2001yj@gmail.com>
2024-09-22 20:32:37 -0500

    se crearon clases de ejemplo, para respetar la estructura del project

---
+++ src/main/java/views/View.java
@@ -0,0 +1,10 @@
+package views;
+
+public class View {
+
+	public static void main(String[] args) {
+		//clase de ejemplo, en este packages se crearan las vistas swing, una vez creadas eliminar esta clase
+		//ya que la primera vista se llamara desde el main
+	}
+
+}
