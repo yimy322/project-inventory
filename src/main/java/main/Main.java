1e65bac1f9718bd8a576a25941fbc08174795c97
Manuel Cruz <cruzriveraedwinmanuel@gmail.com>
2024-09-23 22:52:19 -0500

    Se creo la vista menu

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,12 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import connection.ConnectionDB;
-
+import views.FormMain;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			Connection con = ConnectionDB.getConnection();
-			System.out.println("Prueba de conexion exitosa");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		FormMain main = new FormMain();
+		main.setVisible(true);
 	}
 
 }
---
+++ src/main/java/views/FormMain.java
@@ -0,0 +1,49 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class FormMain extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormMain frame = new FormMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FormMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(58, 72, 143, 44);
+		contentPane.add(textField);
+		textField.setColumns(10);
+	}
+}
