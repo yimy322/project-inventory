5ab6faf693c79a2c6d78c37835bcf62d54a0e157
Yanina Manrique <manriquey66@gmail.com>
2024-10-30 13:57:53 -0500

    se agrego botones para el login

--- src/main/java/views/FormLogin.java
+++ src/main/java/views/FormLogin.java
@@ -12,6 +12,7 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Font;
 import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class FormLogin {
 
@@ -59,35 +60,43 @@ public class FormLogin {
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(51, 153, 204));
-		panel_1.setBounds(209, 49, 379, 384);
+		panel_1.setBounds(215, 40, 398, 399);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Bienvenido");
-		lblNewLabel.setBounds(123, 34, 132, 28);
+		lblNewLabel.setBounds(142, 25, 132, 49);
 		lblNewLabel.setFont(new Font("Arial Narrow", Font.PLAIN, 31));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		panel_1.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(142, 115, 198, 28);
+		textField.setBounds(128, 115, 233, 28);
 		panel_1.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Usuario:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(61, 112, 71, 28);
+		lblNewLabel_1.setBounds(47, 112, 71, 28);
 		panel_1.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Contraseña:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		lblNewLabel_2.setBounds(37, 166, 95, 28);
+		lblNewLabel_2.setBounds(23, 166, 95, 28);
 		panel_1.add(lblNewLabel_2);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(142, 169, 198, 27);
+		passwordField.setBounds(128, 169, 233, 27);
 		panel_1.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.setBounds(112, 249, 161, 34);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Salir");
+		btnNewButton_1.setBounds(112, 310, 161, 34);
+		panel_1.add(btnNewButton_1);
 	}
 }
