386dc0ec14c10ae9761d384fecbb9dd63a379f02
Yanina Manrique <manriquey66@gmail.com>
2024-10-30 14:29:21 -0500

    se termino el diseño del login

--- src/main/java/views/FormLogin.java
+++ src/main/java/views/FormLogin.java
@@ -13,6 +13,8 @@ import java.awt.Rectangle;
 import java.awt.Font;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.SystemColor;
+import javax.swing.border.LineBorder;
 
 public class FormLogin {
 
@@ -54,49 +56,57 @@ public class FormLogin {
 		frame.setLocationRelativeTo(null); //CENTRA EL FRAME
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(70, 130, 180), 1, true));
 		panel.setBackground(new Color(51, 102, 153));
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(51, 153, 204));
-		panel_1.setBounds(215, 40, 398, 399);
+		panel_1.setBounds(215, 40, 386, 399);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Bienvenido");
-		lblNewLabel.setBounds(142, 25, 132, 49);
-		lblNewLabel.setFont(new Font("Arial Narrow", Font.PLAIN, 31));
+		lblNewLabel.setBounds(133, 28, 132, 49);
+		lblNewLabel.setFont(new Font("Arial Narrow", Font.PLAIN, 32));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		panel_1.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(128, 115, 233, 28);
+		textField.setBounds(112, 125, 233, 31);
 		panel_1.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Usuario:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setFont(new Font("Arial Narrow", Font.PLAIN, 17));
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(47, 112, 71, 28);
+		lblNewLabel_1.setBounds(39, 124, 63, 28);
 		panel_1.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Contraseña:");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_2.setFont(new Font("Arial Narrow", Font.PLAIN, 17));
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		lblNewLabel_2.setBounds(23, 166, 95, 28);
+		lblNewLabel_2.setBounds(23, 192, 81, 28);
 		panel_1.add(lblNewLabel_2);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(128, 169, 233, 27);
+		passwordField.setBounds(112, 193, 233, 31);
 		panel_1.add(passwordField);
 		
 		JButton btnNewButton = new JButton("Ingresar");
-		btnNewButton.setBounds(112, 249, 161, 34);
+		btnNewButton.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		btnNewButton.setFont(new Font("Arial Narrow", Font.PLAIN, 14));
+		btnNewButton.setBackground(new Color(173, 216, 230));
+		btnNewButton.setBounds(39, 289, 140, 31);
 		panel_1.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Salir");
-		btnNewButton_1.setBounds(112, 310, 161, 34);
+		btnNewButton_1.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
+		btnNewButton_1.setFont(new Font("Arial Narrow", Font.PLAIN, 14));
+		btnNewButton_1.setBackground(new Color(173, 216, 230));
+		btnNewButton_1.setBounds(215, 289, 140, 31);
 		panel_1.add(btnNewButton_1);
 	}
 }
