1c37d673393e9297be2b3034b25f65149b402d78
yimy <fh2001yj@gmail.com>
2024-12-08 18:19:47 -0500

    se agrego el metodo actualizar listas enlazadas

--- src/main/java/controllers/SalesController.java
+++ src/main/java/controllers/SalesController.java
@@ -38,7 +38,7 @@ public class SalesController implements ActionListener {
 	int cantidadElegida = 0;
 	int cantidadTotal = 0;
 	double totalNeto = 0.00;
-	
+
 	public SalesController(vSales form) {
 		this.form = form;
 		this.form.btnBuscarCliente.addActionListener(this);
@@ -46,6 +46,8 @@ public class SalesController implements ActionListener {
 		this.form.cbProveedor.addActionListener(this);
 		this.form.cbProducto.addActionListener(this);
 		this.form.btnAgregarProducto.addActionListener(this);
+		this.form.btnEliminarTodo.addActionListener(this);
+		this.form.btnEliminarUltimo.addActionListener(this);
 		init();
 	}
 
@@ -58,8 +60,8 @@ public class SalesController implements ActionListener {
 		loadSuppliers();
 		// un default para el combo
 		this.form.cbProducto.addItem(nodeDefault);
-		this.form.txtCantidadProd.setText(cantidadTotal+"");
-		this.form.txtTotalProd.setText(totalNeto+"");
+		this.form.txtCantidadProd.setText(cantidadTotal + "");
+		this.form.txtTotalProd.setText(totalNeto + "");
 	}
 
 	public void searchCustomer() {
@@ -109,7 +111,7 @@ public class SalesController implements ActionListener {
 			form.cbCategoria.addItem(nodeCategoria);
 		}
 	}
-	
+
 	public void loadSuppliers() {
 		LinkedList proveedores = supplierService.findAll();// Se obtiene una lista de proveedores de la base de datos
 		form.cbProveedor.removeAllItems(); // Se limpia el combo
@@ -121,27 +123,27 @@ public class SalesController implements ActionListener {
 			form.cbProveedor.addItem(nodeProveedor); // Se agrega cada proveedor al combo
 		}
 	}
-	
+
 	public void loadProducts() {
 		form.cbProducto.removeAllItems();
 		form.cbProducto.addItem(nodeDefault);
 		for (int i = 0; i < productos.size(); i++) {
 			Product producto = (Product) productos.get(i);
-			NodeHash nodeProducto= new NodeHash(producto.getIdProduct(), producto);
-			//captura los id de los combos
+			NodeHash nodeProducto = new NodeHash(producto.getIdProduct(), producto);
+			// captura los id de los combos
 			int indCategorias = searchIdCbm(form.cbCategoria.getSelectedItem());
 			int indProveedores = searchIdCbm(form.cbProveedor.getSelectedItem());
-			//en caso alguno de ellos tenga dato
-			if(indCategorias>0 || indProveedores>0) {
-				//los dos deben tener data para que se agregue al combo de productos
-				if(producto.getIdCategory()==indCategorias && producto.getIdSupplier()==indProveedores) {
-					//si matchea se agrega. EN CASO CONTRARIO, NO
+			// en caso alguno de ellos tenga dato
+			if (indCategorias > 0 || indProveedores > 0) {
+				// los dos deben tener data para que se agregue al combo de productos
+				if (producto.getIdCategory() == indCategorias && producto.getIdSupplier() == indProveedores) {
+					// si matchea se agrega. EN CASO CONTRARIO, NO
 					form.cbProducto.addItem(nodeProducto);
 				}
 			}
 		}
 	}
-	
+
 	public int searchIdCbm(Object obj) {
 		NodeHash selectedItem = (NodeHash) obj;
 		return selectedItem.key;
@@ -154,77 +156,76 @@ public class SalesController implements ActionListener {
 		this.form.txtTelefono.setText("");
 		this.form.txtEmail.setText("");
 	}
-	
+
 	public void loadQuantity() {
-		if(this.form.cbProducto.getSelectedItem()!=null) {
-			if(!this.form.cbProducto.getSelectedItem().equals(nodeDefault)) {
+		if (this.form.cbProducto.getSelectedItem() != null) {
+			if (!this.form.cbProducto.getSelectedItem().equals(nodeDefault)) {
 				this.form.txtCantidad.setEditable(true);
-				NodeHash node = (NodeHash)this.form.cbProducto.getSelectedItem();
+				NodeHash node = (NodeHash) this.form.cbProducto.getSelectedItem();
 				Product producto = (Product) node.value;
 				cantidad = producto.getQuantity();
-				this.form.lblMaximo.setText("Max. "+cantidad);
-			}else {
+				this.form.lblMaximo.setText("Max. " + cantidad);
+			} else {
 				this.form.txtCantidad.setEditable(false);
 				this.form.lblMaximo.setText("Max. 0");
 				cantidad = 0;
 			}
-		}	
+		}
 	}
 
 	public void addProduct() {
-		if(this.form.txtCantidad.getText().trim().isEmpty()) {
-			JOptionPane.showMessageDialog(null, "Debe indicar la cantidad", "Ventas",
-					JOptionPane.WARNING_MESSAGE);
+		if (this.form.txtCantidad.getText().trim().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Debe indicar la cantidad", "Ventas", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
 		int cantidadActual = Integer.parseInt(this.form.txtCantidad.getText());
-		if(cantidadActual<0) {
+		if (cantidadActual < 0) {
 			JOptionPane.showMessageDialog(null, "La cantidad no puede ser negativa", "Ventas",
 					JOptionPane.WARNING_MESSAGE);
 			return;
 		}
-		if(cantidadActual>cantidad) {
+		if (cantidadActual > cantidad) {
 			JOptionPane.showMessageDialog(null, "La cantidad no puede sobrepasar a la cantidad maxima", "Ventas",
 					JOptionPane.WARNING_MESSAGE);
 			return;
 		}
 		cantidadElegida = cantidadActual;
-		//logica para agregar el producto
+		// logica para agregar el producto
 		int idProducto = searchIdCbm(form.cbProducto.getSelectedItem());
-		//capturar desde la lista
+		// capturar desde la lista
 		Product nuevoProducto = searchProductById(idProducto);
 		addProducts(nuevoProducto);
 		loadProducts();
 		this.form.txtCantidad.setText("");
 	}
-	
+
 	public Product searchProductById(int idProducto) {
 		for (int i = 0; i < productos.size(); i++) {
 			Product producto = (Product) productos.get(i);
-			if(producto.getIdProduct()==idProducto) {
+			if (producto.getIdProduct() == idProducto) {
 				return producto;
 			}
 		}
 		return null;
 	}
-	
+
 	public void addProducts(Product prod) {
-		//se creo una copia del nuevo producto
+		// se creo una copia del nuevo producto
 		Product nuevoProducto = new Product(prod);
-		//se calcula el total
+		// se calcula el total
 		double total = prod.getPrice() * cantidadElegida;
-		//se calcula la cantidad restante
-		cantidad= prod.getQuantity() - cantidadElegida;
-		//aca se actualiza la cantidad del producto
+		// se calcula la cantidad restante
+		cantidad = prod.getQuantity() - cantidadElegida;
+		// aca se actualiza la cantidad del producto
 		nuevoProducto.setQuantity(cantidad);
 		productos.update(prod, nuevoProducto);
-		productos.print();	
-		//-------
-		this.form.lblMaximo.setText("Max. "+cantidad);
-		//verificamos que en caso el producto exista se incremente la cantidad
+		productos.print();
+		// -------
+		this.form.lblMaximo.setText("Max. " + cantidad);
+		// verificamos que en caso el producto exista se incremente la cantidad
 		boolean productoExistente = existProduct(prod);
-		//---
-		if(!productoExistente) {
+		// ---
+		if (!productoExistente) {
 			Object ob[] = new Object[8];
 			ob[0] = prod.getIdProduct();
 			ob[1] = prod.getName();
@@ -235,57 +236,92 @@ public class SalesController implements ActionListener {
 			this.form.model.addRow(ob);
 		}
 		this.form.jTableProducts.setModel(this.form.model);
-		//actualiza la cantidad
-		updateQuantity(cantidadElegida);
+		// actualiza la cantidad
+		updateQuantity();
 		updateTotal();
 	}
-	
+
 	public boolean existProduct(Product prod) {
 		for (int i = 0; i < this.form.model.getRowCount(); i++) {
-	        int idProductoTabla = (int) this.form.model.getValueAt(i, 0); // capturamos el id en la primera columna
-	        if (idProductoTabla == prod.getIdProduct()) {
-	            //si existe se incremente la cantidad
-	            int cantidadExistente = (int) this.form.model.getValueAt(i, 4);
-	            this.form.model.setValueAt(cantidadExistente + cantidadElegida, i, 4); //actualizamos la cantidad
-	            this.form.model.setValueAt((cantidadExistente + cantidadElegida) * prod.getPrice(), i, 5); //actualizamos el total
-	            return true;
-	        }
-	    }
+			int idProductoTabla = (int) this.form.model.getValueAt(i, 0); // capturamos el id en la primera columna
+			if (idProductoTabla == prod.getIdProduct()) {
+				// si existe se incremente la cantidad
+				int cantidadExistente = (int) this.form.model.getValueAt(i, 4);
+				this.form.model.setValueAt(cantidadExistente + cantidadElegida, i, 4); // actualizamos la cantidad
+				this.form.model.setValueAt((cantidadExistente + cantidadElegida) * prod.getPrice(), i, 5); // actualizamos
+																											// el total
+				return true;
+			}
+		}
 		return false;
 	}
-	
-	public void updateQuantity(int cant) {
-		cantidadTotal+=cant;
-		this.form.txtCantidadProd.setText(cantidadTotal+"");
+
+	public void updateQuantity() {
+		cantidadTotal = 0;
+		for (int i = 0; i < this.form.model.getRowCount(); i++) {
+			int cantPorProducto = (int) this.form.model.getValueAt(i, 4);
+			cantidadTotal += cantPorProducto;
+		}
+		this.form.txtCantidadProd.setText(cantidadTotal + "");
 	}
-	
+
 	public void updateTotal() {
 		totalNeto = 0.00;
 		for (int i = 0; i < this.form.model.getRowCount(); i++) {
 			double totalPorProducto = (double) this.form.model.getValueAt(i, 5);
 			totalNeto += totalPorProducto;
-	    }
-		this.form.txtTotalProd.setText(totalNeto+"");
+		}
+		this.form.txtTotalProd.setText(totalNeto + "");
 	}
-	
+
+	public void deleteAll() {
+		for (int j = 0; j < this.form.jTableProducts.getRowCount(); j++) {
+			this.form.model.removeRow(j);
+			j -= 1;
+		}
+		productos = productService.findAll();
+		loadProducts();
+		cantidadTotal = 0;
+		totalNeto = 0.00;
+		this.form.txtCantidadProd.setText(cantidadTotal + "");
+		this.form.txtTotalProd.setText(totalNeto + "");
+	}
+
+	public void deleteRow() {
+		int rowCount = this.form.model.getRowCount();
+		if (rowCount > 0) {
+			int id = (int) this.form.model.getValueAt(rowCount - 1, 0);
+			Product productoActual = searchProductById(id);
+			Product productoNuevo = productService.selectById(id);
+			productos.update(productoActual, productoNuevo);
+			updateQuantity();
+			updateTotal();
+			this.form.model.removeRow(rowCount - 1); // elimina la ultima fila
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		Object press = ae.getSource();
 		if (press == this.form.btnBuscarCliente) {
 			searchCustomer();
 		} else if (press == this.form.cbCategoria) {
-			//si no se selecciono nada que no busque
-			if(!this.form.cbCategoria.getSelectedItem().equals(nodeDefault))
+			// si no se selecciono nada que no busque
+			if (!this.form.cbCategoria.getSelectedItem().equals(nodeDefault))
 				loadProducts();
 		} else if (press == this.form.cbProveedor) {
-			//si no se selecciono nada que no busque
-			if(!this.form.cbProveedor.getSelectedItem().equals(nodeDefault))
+			// si no se selecciono nada que no busque
+			if (!this.form.cbProveedor.getSelectedItem().equals(nodeDefault))
 				loadProducts();
-		}else if (press == this.form.cbProducto) {
-			//si no se selecciono nada que no busque
+		} else if (press == this.form.cbProducto) {
+			// si no se selecciono nada que no busque
 			loadQuantity();
-		}else if (press == this.form.btnAgregarProducto) {
+		} else if (press == this.form.btnAgregarProducto) {
 			addProduct();
+		} else if (press == this.form.btnEliminarTodo) {
+			deleteAll();
+		} else if (press == this.form.btnEliminarUltimo) {
+			deleteRow();
 		}
 	}
 
